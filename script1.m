0f8489a47939693e435c675d1e0246806a451cfd
DanielSF1 <132016363+DanielSF1@users.noreply.github.com>
2023-04-28 10:14:32 +0200

    Update script1.m

--- script1.m
+++ script1.m
@@ -4,38 +4,32 @@ clear all;
 #graphics_toolkit ("gnuplot");
 graphics_toolkit ("qt");
 
-#ejecuciones=20;
-#datos=load('resultados_chr18b_2');
-#ejecuciones=20;
-#datos=load('resultados_chr20a_2');
-#ejecuciones=20;
-#datos=load('resultados_chr20b_2');
-#ejecuciones=20;
-#datos=load('resultados_esc32a_2');
-#ejecuciones=20;
-#datos=load('resultados_had20_2');
-#ejecuciones=25;
-#datos=load('resultados_Rou20_2');
-#ejecuciones=20;
-#datos=load('resultados_tai20b_2');
+#executions=20;
+#data=load('results_chr18b_2');
+#data=load('results_chr20a_2');
+#data=load('results_chr20b_2');
+#data=load('results_esc32a_2');
+#data=load('results_had20_2');
+#data=load('results_Rou20_2');
+#data=load('results_tai20b_2');
 
-ejecuciones=20;
-datos1=load('Resultados_bur_3');
-datos2=load('Resultados_chr18b_3');
-datos3=load('Resultados_chr20a_3');
-datos4=load('Resultados_chr20b_3');
-datos5=load('Resultados_els19_3');
-datos6=load('Resultados_esc32a_3');
-datos7=load('Resultados_esc32e_3');
-datos8=load('Resultados_had20_3');
-datos9=load('Resultados_nug20_3');
-datos10=load('Resultados_rou20_3');
-datos11=load('Resultados_scr_20_3');
-datos12=load('Resultados_tai20b_3');
+executions=20;
+data1=load('Results_bur_3');
+data2=load('Results_chr18b_3');
+data3=load('Results_chr20a_3');
+data4=load('Results_chr20b_3');
+data5=load('Results_els19_3');
+data6=load('Results_esc32a_3');
+data7=load('Results_esc32e_3');
+data8=load('Results_had20_3');
+data9=load('Results_nug20_3');
+data10=load('Results_rou20_3');
+data11=load('Results_scr_20_3');
+data12=load('Results_tai20b_3');
 
-#d2=sortrows(datos8,1);
+#d2=sortrows(data8,1);
 #d3=d2(:,2);
-#d4=reshape(d3,ejecuciones,2000);
+#d4=reshape(d3,executions,2000);
 
 #subplot(2,1,1);
 #plot(mean(d4));
@@ -46,73 +40,73 @@ datos12=load('Resultados_tai20b_3');
 #boxplot(d4(:,2000));
 
 subplot(4,3,1);
-boxplot((reshape(sortrows(datos1,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data1,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("bur26a")
 subplot(4,3,2);
-boxplot((reshape(sortrows(datos2,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data2,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("chr18b")
 subplot(4,3,3);
-boxplot((reshape(sortrows(datos3,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data3,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("chr20a")
 subplot(4,3,4);
-boxplot((reshape(sortrows(datos4,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data4,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("chr20b")
 subplot(4,3,5);
-boxplot((reshape(sortrows(datos5,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data5,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("els19")
 subplot(4,3,6);
-boxplot((reshape(sortrows(datos6,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data6,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("esc32a")
 subplot(4,3,7);
-boxplot((reshape(sortrows(datos7,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data7,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("esc32e")
 subplot(4,3,8);
-boxplot((reshape(sortrows(datos8,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data8,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("had20")
 subplot(4,3,9);
-boxplot((reshape(sortrows(datos9,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data9,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("nug20")
 subplot(4,3,10);
-boxplot((reshape(sortrows(datos10,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data10,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("rou20")
 subplot(4,3,11);
-boxplot((reshape(sortrows(datos11,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data11,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
 title("scr20")
 subplot(4,3,12);
-boxplot((reshape(sortrows(datos12,1)(:,2),ejecuciones,2000)(:,2000)));
+boxplot((reshape(sortrows(data12,1)(:,2),executions,2000)(:,2000)));
 axis([0.5 1.5])
 xlabel("Last Iteration")
 ylabel("Fitness")
@@ -120,73 +114,73 @@ title("tai20b")
 
 
 ##subplot(4,3,1);
-##plot(mean(reshape(sortrows(datos1,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data1,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("bur26a")
 ##
 ##subplot(4,3,2);
-##plot(mean(reshape(sortrows(datos2,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data2,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("chr18b")
 ##
 ##subplot(4,3,3);
-##plot(mean(reshape(sortrows(datos3,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data3,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("chr20a")
 ##
 ##subplot(4,3,4);
-##plot(mean(reshape(sortrows(datos4,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data4,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("chr20b")
 ##
 ##subplot(4,3,5);
-##plot(mean(reshape(sortrows(datos5,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data5,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("els19")
 ##
 ##subplot(4,3,6);
-##plot(mean(reshape(sortrows(datos6,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data6,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("esc32a")
 ##
 ##subplot(4,3,7);
-##plot(mean(reshape(sortrows(datos7,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data7,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("esc32e")
 ##
 ##subplot(4,3,8);
-##plot(mean(reshape(sortrows(datos8,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data8,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("had20")
 ##
 ##subplot(4,3,9);
-##plot(mean(reshape(sortrows(datos9,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data9,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("nug20")
 ##
 ##subplot(4,3,10);
-##plot(mean(reshape(sortrows(datos10,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data10,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("rou20")
 ##
 ##subplot(4,3,11);
-##plot(mean(reshape(sortrows(datos11,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data11,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("scr20")
 ##
 ##subplot(4,3,12);
-##plot(mean(reshape(sortrows(datos12,1)(:,2),ejecuciones,2000)),'linewidth',3);
+##plot(mean(reshape(sortrows(data12,1)(:,2),executions,2000)),'linewidth',3);
 ##xlabel("Iterations")
 ##ylabel("Fitness")
 ##title("tai20b")
